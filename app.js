4c9411f558bfb243b996062d07d67be175843a09
danilocbueno <danilocbueno@gmail.com>
2021-01-13 11:48:20 -0300

    importanto o bodyparser para fazer o parser dos forms

--- app.js
+++ app.js
@@ -20,9 +20,4 @@ app.use('/product', (req, res, next) => {
   res.redirect('/');
 });
 
-app.use((req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
-
 app.listen(3000);
