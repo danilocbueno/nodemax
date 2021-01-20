4276a59886eb065ab6ca59151f48a766d39bcdcb
danilocbueno <danilocbueno@gmail.com>
2021-01-20 11:00:12 -0300

    finalizando o modulo; adicionando novas rotas e paginas

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,11 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
+    const imageURL = req.body.imageURL;
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(title, imageURL, description, price);
     
     product.save();
     res.redirect('/');
