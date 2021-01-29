baec2bb76540207dd93c17414320d9e380c75802
danilocbueno <danilocbueno@gmail.com>
2021-01-29 20:36:41 -0300

    try new deploy

---
+++ config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "development": {
+        "username": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "host": process.env.DB_HOST,
+        "database": process.env.DB_DATABASE,
+        "dialect": "mysql"
+    },
+    "test": {
+        "database": "products_api_test",
+        "dialect": "postgres"
+    },
+    "production": {
+        "use_env_variable": "DATABASE_URL",
+        "dialect": "postgres",
+        "dialectOptions": {
+            "ssl": true
+        }
+    }
+};
--- util/database.js
+++ util/database.js
@@ -1,7 +1,21 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('nodemax', 'root', 'qwe123456', {
+
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+/*
+const sequelize = new Sequelize(
+    'nodemax',
+    'root',
+    'qwe123456', {
     dialect: 'mysql',
     host: 'localhost'
 });
+*/
 
 module.exports = sequelize;
